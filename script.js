9693ada7e0b33666bf43e3e1b1bc0c1b5d3290f9
me <me@test.com>
2024-05-21 18:33:18 +0800

    Add some changes

--- script.js
+++ script.js
@@ -11,24 +11,26 @@ function Gameboard() {
 
     const dropToken = (player, position) => {
         const isAvailableCell =
-            (board[position.row][position.column].getValue() === '') ?
+            (board[position.row][position.column].getValue() === null) ?
                 true : false;
-        if (!isAvailableCell) return;
+        if (!isAvailableCell) return false;
         board[position.row][position.column].changeValue(player);
+        return true;
     }
 
     return {getBoard, dropToken};
 }
 
 function Cell(row, column) {
-    let value = '';
+    let value = null;
     const getValue = () => value;
     const changeValue = (player) => value = player.token;
+    const clearValue = () => value = null;
 
     const position = {row, column};
     const getPosition = () => position;
 
-    return {getValue, changeValue, getPosition};
+    return {getValue, changeValue, clearValue, getPosition};
 }
 
 function Game() {
@@ -42,18 +44,21 @@ function Game() {
     const switchActivePlayer = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0];
     }
-    let turn = 1;
-    let X = 0;
-    let O = 0;
+    let scores = {X: 0, O: 0};
     const playRound = (position) => {
-        gameboard.dropToken(getActivePlayer(), position);
-        let winner = referee();
-        if (winner && turn <= 3) {
-            winner = 'X' ? X++ : O++;
-            turn++;
-            return {X, O};
+        let wasDropped = gameboard.dropToken(getActivePlayer(), position);
+        let roundWinner = referee();
+        if (roundWinner && roundWinner !== "tie") {
+            scores[roundWinner]++;
+            gameboard.getBoard().forEach(row => {
+                row.forEach(cell => {
+                    cell.clearValue();
+                });
+            });
+        } else if (wasDropped) {
+            switchActivePlayer();
         }
-        switchActivePlayer();
+        return {roundWinner, scores};
     }
     const referee = () => {
         const board = gameboard.getBoard();
@@ -70,13 +75,20 @@ function Game() {
             [board[0][0], board[1][1], board[2][2]],
             [board[2][0], board[1][1], board[0][2]]
         ];
+
         for (const line of lines) {
             const [a, b, c] = line;
-            if (a.getValue() === b.getValue() && b.getValue() === c.getValue()){
+            if (a.getValue() === b.getValue() && b.getValue() === c.getValue() &&
+                a.getValue() && b.getValue() && c.getValue()){
                 return a.getValue();
             }
-            return null;
         }
+
+        const full = function() {
+            return gameboard.getBoard().every(row => row.every(cell => cell.getValue() !== ''));
+        };
+        if (full()) return "tie";
+        return null;
     }
 
     return {getBoard: gameboard.getBoard, getActivePlayer, playRound};
@@ -86,7 +98,7 @@ function screenController() {
     const game = Game();
     const messageDiv = document.querySelector('.message');
     const boardDiv = document.querySelector('.board');
-    let scores = {X:0, O:0};
+    let scores = {X: 0, O: 0};
     let winner = null;
     const updateScreen = () => {
         boardDiv.replaceChildren();
@@ -95,25 +107,26 @@ function screenController() {
         if (scores.X === 3) winner = 'X';
         if (scores.O === 3) winner = 'O';
         if (!winner) {
-            messageDiv.textContent = activePlayer.token +`'s Turn `+
-                "X:" + scores.X + "  O:" + scores.O;
+            messageDiv.textContent = activePlayer.token +`'s Turn (`+
+                "X:" + scores.X + " O:" + scores.O + ")";
         } else {
             messageDiv.textContent = winner + " WINS!";
-            scores = {X:0, O:0};
+            scores = {X: 0, O: 0};
         }
         board.forEach(row => {
             row.forEach(cell => {
-            const cellButton = document.createElement('button');
-            cellButton.classList.add('cell');
-            cellButton.textContent = cell.getValue();
-            cellButton.addEventListener('click', () => {
-                scores = game.playRound(cell.getPosition());
-                updateScreen();
-                }
-            );
-            boardDiv.append(cellButton);
-            })
-        })
+                const cellButton = document.createElement('button');
+                cellButton.classList.add('cell');
+                cellButton.textContent = cell.getValue();
+                cellButton.addEventListener('click', () => {
+                    scores = game.playRound(cell.getPosition()).scores;
+                    let roundWinner = game.playRound(cell.getPosition()).roundWinner;
+                    updateScreen();
+                    }
+                );
+                boardDiv.append(cellButton);
+            });
+        });
     }
     return {updateScreen};
 }
